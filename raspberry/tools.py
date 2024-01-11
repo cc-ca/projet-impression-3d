cee63135527be5788db8648869740f86eac21ce2
unknown <maxime.herbin0545@laposte.net>
2024-01-11 11:15:27 +0100

    fix

--- raspberry/tools.py
+++ raspberry/tools.py
@@ -13,6 +13,7 @@ import datetime
 
 def load_and_preprocess_image(image_path):
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    plt.imshow(img)
     img = cv2.resize(img, (255, 255))  # Assurez-vous que la taille correspond à celle utilisée lors de l'entraînement
     img = img / 255.0  # Normalisez les valeurs des pixels
     img = np.expand_dims(img, axis=0)  # Ajoutez une dimension pour représenter le lot (batch)
