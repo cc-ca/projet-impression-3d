c5090b435051dce0172f9179effe5a4c52b7cc32
cc-ca <celestin.captal@student.junia.com>
2024-01-11 09:57:06 +0100

    script ras

---
+++ raspberry/tools.py
@@ -0,0 +1,65 @@
+from PIL import Image 
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+import cv2
+import random
+import time
+import datetime
+
+def load_and_preprocess_image(image_path):
+    img = cv2.imread(image_path)
+    img = cv2.resize(img, (255, 255))  # Assurez-vous que la taille correspond à celle utilisée lors de l'entraînement
+    img = img / 255.0  # Normalisez les valeurs des pixels
+    img = np.expand_dims(img, axis=0)  # Ajoutez une dimension pour représenter le lot (batch)
+
+    return img
+
+def predict_defect_multi_class(model, image_path):
+    preprocessed_img = load_and_preprocess_image(image_path)
+    predictions = model.predict(preprocessed_img)
+    #print (model.predict(preprocessed_img))
+
+    # Interprétez les prédictions
+    class_labels = [ 'OK','bed_not_stick', 'spaghetti']
+    predicted_class = class_labels[np.argmax(predictions)]
+
+    return predicted_class
+
+
+
+def capture(model):
+  # Ouvrir la webcam (la webcam par défaut a l'ID 0)
+  cap = cv2.VideoCapture(0)
+  # Vérifier si la webcam est ouverte correctement
+  if not cap.isOpened():
+      print("Erreur: Impossible d'ouvrir la webcam.")
+      exit()
+  # Capturer une image
+  ret, frame = cap.read()
+
+  if ret:
+      cv2.imwrite("photo_capturee.jpg", frame)
+      print("Photo capturée avec succès.")
+      result = predict_defect_multi_class(model, 'photo_capturee.jpg')
+      img = Image.open('photo_capturee.jpg')
+      #plt.imshow(img)
+      #plt.axis('off')  # Masquer les axes
+      #now = datetime.datetime.now().strftime("%H:%M:%S")
+      #plt.show()
+      cap.release()
+      return(result)
+    
+    
+
+  else:
+      print("Erreur lors de la capture de la photo.")
+      cap.release()
+      return('error')
+
+  # Libérer la webcam
+  
