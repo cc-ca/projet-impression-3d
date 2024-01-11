e4af8d92037030bdf4a8a59a08fb7fc9f6315eee
unknown <maxime.herbin0545@laposte.net>
2024-01-11 14:47:11 +0100

    Update script.py

--- raspberry/script.py
+++ raspberry/script.py
@@ -3,13 +3,21 @@ import time
 import cv2
 from tensorflow.keras.models import load_model
 import tools
+from enum import Enum
 
+# Define an enumeration class
+class Color(Enum):
+    OFF = 0
+    IDLE = 1
+    ERROR = 2
+    CORRECT = 3
+    
 # Définir les broches GPIO pour chaque couleur de la LED et le bouton
 pin_red = 19
 pin_green = 13
 pin_blue = 26
 pin_button = 6
-model = load_model('model.h5')
+
 
 # Initialiser la bibliothèque GPIO
 GPIO.setmode(GPIO.BCM)
@@ -18,39 +26,71 @@ GPIO.setup(pin_green, GPIO.OUT)
 GPIO.setup(pin_blue, GPIO.OUT)
 GPIO.setup(pin_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Constants
+RUN_DURATION = 300
+SLEEP_INTERVAL = 5
+CONFIDENCE_THRESHOLD = 0.9
+model = load_model('model.h5')
+
+
 def change_color(color):
     GPIO.output(pin_red, GPIO.LOW)
     GPIO.output(pin_green, GPIO.LOW)
     GPIO.output(pin_blue, GPIO.LOW)
 
-    if color == "red":
+    if color == Color.ERROR:
         GPIO.output(pin_red, GPIO.HIGH)
-    elif color == "green":
+    elif color == Color.CORRECT:
         GPIO.output(pin_green, GPIO.HIGH)
-    elif color == "blue":
+    elif color == Color.IDLE:
         GPIO.output(pin_blue, GPIO.HIGH)
 
-try:
-    print("Programme en cours")
-    # Ouvrir la webcam (la webcam par défaut a l'ID 0)
-    cap = cv2.VideoCapture(0)
-    change_color("blue")
-    time.sleep(2)
-    consecutive_failures = 0
-    
-    while True:
-        result = tools.capture(model, cap)  # Appel du modele d'IA
-        print(result)
-        if(result == "OK"):
-            change_color("green")
-        else :
-            change_color("red")
-        time.sleep(5)
+def evaluate_model(cap):
+    result = tools.capture(model, cap)
+    print(result)
 
-except KeyboardInterrupt:
-    pass
-finally:
-    print("Fin programme")
-    cap.release()
-    change_color("off")
-    GPIO.cleanup()
+    if result == "OK":
+        return Color.CORRECT
+    else:
+        return Color.ERROR
+
+def run():
+    try:
+        cap = cv2.VideoCapture(0) # Ouvrir la webcam (la webcam par défaut a l'ID 0)
+        while True:
+            end_time = time.time() + RUN_DURATION
+            success_count = 0
+            failure_count = 0
+            
+            while time.time() < end_time:
+                color = evaluate_model(cap)
+                change_color(Color.OFF)
+                change_color(color)
+    
+                if color == Color.ERROR:
+                    failure_count += 1
+                elif color == Color.CORRECT:
+                    success_count += 1
+                    
+                time.sleep(SLEEP_INTERVAL)
+                
+            if failure_count/(success_count+failure_count) >= CONFIDENCE_THRESHOLD:
+                break
+    finally:
+        cap.release()
+        change_color(Color.IDLE)
+        
+
+
+if __name__ == "__main__":
+    try:
+        print("Programme en cours")
+        change_color(Color.IDLE)
+        while True:
+            if(GPIO.input(pin_button) == GPIO.LOW):
+                run()
+    except KeyboardInterrupt:
+        change_color(Color.OFF)
+        GPIO.cleanup()
+        print("Fin programme")
+    
