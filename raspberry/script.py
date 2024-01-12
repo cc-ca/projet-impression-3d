bdcefb960f9bee167f993326dbbffc2356d91a98
P0TEZ <root@LY0681>
2024-01-12 11:35:33 +0100

    queue system for result history

--- raspberry/script.py
+++ raspberry/script.py
@@ -1,22 +1,23 @@
 import RPi.GPIO as GPIO
 import time
+import cv2
 from tensorflow.keras.models import load_model
 import tools
 from enum import Enum
+from collections import deque
 
+# Define an enumeration class
 class Color(Enum):
     OFF = 0
     IDLE = 1
     ERROR = 2
     CORRECT = 3
-    
+
 # Définir les broches GPIO pour chaque couleur de la LED et le bouton
 pin_red = 19
 pin_green = 13
 pin_blue = 26
 pin_button = 6
-pin_switch_off = 5
-
 
 # Initialiser la bibliothèque GPIO
 GPIO.setmode(GPIO.BCM)
@@ -24,19 +25,16 @@ GPIO.setup(pin_red, GPIO.OUT)
 GPIO.setup(pin_green, GPIO.OUT)
 GPIO.setup(pin_blue, GPIO.OUT)
 GPIO.setup(pin_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(pin_switch_off, GPIO.OUT)
 
-# Constantes
+# Constants
 RUN_DURATION = 60
 SLEEP_INTERVAL = 5
 SLEEP_LED = 0.1
 CONFIDENCE_THRESHOLD = 0.9
 model = load_model('model.h5')
 
-def switch_off():
-    GPIO.ouptut(pin_switch_off, GPIO.HIGH)
-    time.sleep(3)
-    GPIO.output(pin_switch_off, GPIO.LOW)
+# Initialize history
+history = deque(maxlen=300)  # Assuming a measurement every second for 5 minutes
 
 def change_color(color):
     GPIO.output(pin_red, GPIO.LOW)
@@ -54,6 +52,9 @@ def evaluate_model():
     result = tools.capture(model)
     print(result)
 
+    current_time = time.localtime()
+    history.append((result, current_time))
+    
     if result == "OK":
         return Color.CORRECT
     else:
@@ -79,25 +80,23 @@ def run():
                     
                 time.sleep(SLEEP_INTERVAL)
                 
-            if failure_count/(success_count+failure_count) >= CONFIDENCE_THRESHOLD:
-                print("depassement seuil")
+            error_rate = failure_count / (success_count + failure_count)
+            if error_rate >= CONFIDENCE_THRESHOLD:
+                print("Depassement du seuil - Taux d'erreur : {:.2%}".format(error_rate))
                 break
-            print("pas depassement seuil")
+            print("Pas de dépassement du seuil - Taux d'erreur : {:.2%}".format(error_rate))
     finally:
         change_color(Color.IDLE)
-        
-
 
 if __name__ == "__main__":
     try:
         print("Programme en cours")
         change_color(Color.IDLE)
         while True:
-            if(GPIO.input(pin_button) == GPIO.LOW):
+            if GPIO.input(pin_button) == GPIO.LOW:
                 run()
             time.sleep(0.1)
     except KeyboardInterrupt:
         change_color(Color.OFF)
         GPIO.cleanup()
-        print("Fin programme")
-    
+        print("Fin du programme")
