a63aed520715b63af4d70610a5cbb31aacba9253
unknown <maxime.herbin0545@laposte.net>
2024-05-14 11:19:14 +0200

    ajout multithreading et gestion bouton

--- raspberry/script.py
+++ raspberry/script.py
@@ -1,9 +1,11 @@
 import RPi.GPIO as GPIO
+import os, sys
 import time
 from tensorflow.keras.models import load_model
 import tools
 from enum import Enum
 from collections import deque
+import threading
 
 # Define an enumeration class
 class Color(Enum):
@@ -12,13 +14,14 @@ class Color(Enum):
     ERROR = 2
     CORRECT = 3
 
-# Définir les broches GPIO pour chaque couleur de la LED et le bouton
+# Define GPIO pins for each LED color and the button
 pin_red = 19
 pin_green = 13
 pin_blue = 26
 pin_button = 6
+pin_relais = 5
 
-# Initialiser la bibliothèque GPIO
+# Initialize GPIO library
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin_red, GPIO.OUT)
 GPIO.setup(pin_green, GPIO.OUT)
@@ -32,8 +35,9 @@ SLEEP_LED = 0.1
 CONFIDENCE_THRESHOLD = 0.9
 model = load_model('model.h5')
 
-# Initialize history
-history = deque(maxlen=(RUN_DURATION/(SLEEP_INTERVAL)))  # Assuming a measurement every 5 seconds for 1 minute
+# Global variables
+history = deque(maxlen=(RUN_DURATION // SLEEP_INTERVAL))
+is_running = False
 
 def change_color(color):
     GPIO.output(pin_red, GPIO.LOW)
@@ -48,11 +52,10 @@ def change_color(color):
         GPIO.output(pin_blue, GPIO.HIGH)
 
 def evaluate_model():
+    global history
     result = tools.capture(model)
     print(result)
-
-    current_time = time.localtime()
-    history.append((result, current_time))
+    history.append(result)
 
     if result == "0":
         return Color.CORRECT
@@ -60,43 +63,73 @@ def evaluate_model():
         return Color.ERROR
 
 def run():
+    global is_running
+    global history
     try:
         while True:
-            end_time = time.time() + RUN_DURATION
-            
-            while time.time() < end_time:
+            while is_running:
                 color = evaluate_model()
                 change_color(Color.OFF)
                 time.sleep(SLEEP_LED)
                 change_color(color)
-                time.sleep(SLEEP_INTERVAL)
 
-            # Check if there are enough elements in history for calculation
-            if len(history) >= (RUN_DURATION/(SLEEP_INTERVAL)-1):
-                success_count = history.count("OK")
-                failure_count = history.count("ERROR")
-                error_rate = failure_count / (success_count + failure_count)
-                if error_rate >= CONFIDENCE_THRESHOLD:
-                    print("Depassement du seuil - Taux d'erreur : {:.2%}".format(error_rate))
-                    change_color(Color.ERROR)
-                    time.sleep(5)
-                    break
-                print("Pas de dépassement du seuil - Taux d'erreur : {:.2%}".format(error_rate))
-            else:
-                print("Pas assez d'éléments dans l'historique pour le calcul.")
+                # Check if there are enough elements in history for calculation
+                if len(history) >= (RUN_DURATION // SLEEP_INTERVAL - 1):
+                    success_count = history.count("OK")
+                    failure_count = history.count("ERROR")
+                    error_rate = failure_count / (success_count + failure_count)
+                    if error_rate >= CONFIDENCE_THRESHOLD:
+                        print("Threshold exceeded - Error rate: {:.2%}".format(error_rate))
+                        change_color(Color.ERROR)
+                        time.sleep(SLEEP_INTERVAL)
+                        break
+                    print("No threshold exceeded - Error rate: {:.2%}".format(error_rate))
+                else:
+                    print("Not enough elements in history for calculation.")
+                time.sleep(SLEEP_INTERVAL)
     finally:
         change_color(Color.IDLE)
+
+def button_listener():
+    global is_running
+    model_thread = None
+    button_press_start_time = None
+    while True:
+        if GPIO.input(pin_button) == GPIO.LOW:
+            if button_press_start_time is None:
+                button_press_start_time = time.time()
+            else:
+                if time.time() - button_press_start_time >= 3:
+                    print("Restarting script...")
+                    # Cleanup GPIO
+                    GPIO.cleanup()
+                    # Restart the script
+                    python = sys.executable
+                    os.execl(python, python, *sys.argv)
+            time.sleep(0.1)  # Debounce delay
+            if not is_running:
+                if model_thread is not None and model_thread.is_alive():
+                    is_running = False
+                else:
+                    is_running = True
+                    model_thread = threading.Thread(target=run)
+                    model_thread.start()
+            else:
+                is_running = False
+            time.sleep(0.5)  # Delay to handle multiple button presses
+        else:
+            button_press_start_time = None
 
 
 if __name__ == "__main__":
     try:
-        print("Programme en cours")
+        print("Program running")
         change_color(Color.IDLE)
+        # Start button listener in a separate thread
+        threading.Thread(target=button_listener).start()
         while True:
-            if GPIO.input(pin_button) == GPIO.LOW:
-                run()
-            time.sleep(0.1)
+            time.sleep(1)  # Keep the main thread running
     except KeyboardInterrupt:
         change_color(Color.OFF)
         GPIO.cleanup()
-        print("Fin du programme")
+        print("Program ended")
