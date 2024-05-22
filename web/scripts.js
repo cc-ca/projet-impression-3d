2fe328adbdd80c6a58a938ad5b98a16187f52979
P0TEZ <alexpotez@gmail.com>
2024-05-22 09:59:54 +0200

    edit diagram after fecting

--- web/scripts.js
+++ web/scripts.js
@@ -3,6 +3,7 @@ const reloadButton = document.getElementById("reload");
 const statusCircle = document.getElementById("status-circle");
 const statusName = document.getElementById("status-name");
 const printerImage = document.getElementById("printer-image");
+const circularDiagram = document.getElementById("circular-diagram");
 
 const host = window.location.href;
 const formatedHost = host.endsWith("/") ? host.slice(0, -1) : host;
@@ -61,6 +62,8 @@ const fetchData = async () => {
         statusName.textContent =
           status.charAt(0).toUpperCase() + status.slice(1).toLowerCase();
         statusName.style.color = `var(--${MapStatusAndColor[status]})`;
+
+        updateDiagram(data.errorRate);
       });
   } catch (error) {
     console.log("Error fetching data: ", error);
@@ -75,6 +78,65 @@ function updateImage() {
     printerImage.src = `${IMAGE_URL}?${timestamp}`;
   } catch (error) {
     console.log("Error updating image: ", error);
+  }
+}
+
+function updateDiagram(errorRate) {
+  try {
+    const MAX_ERROR_RATE = 0.75; // Maximum error rate allowed (50%)
+
+    const successRate = (1 - errorRate) * 100;
+    const successSize = (successRate * 360) / 100;
+
+    console.log("Success rate: ", successRate);
+    console.log("Success size: ", successSize);
+
+    const orangeStart = MAX_ERROR_RATE * 360 - 1;
+    const orangeEnd = orangeStart + 2;
+
+    console.log("Orange start: ", orangeStart);
+    console.log("Orange end: ", orangeEnd);
+
+    const redStart = successSize;
+    const redEnd = 360;
+
+    console.log("Red start: ", redStart);
+    console.log("Red end: ", redEnd);
+
+    let gradient;
+    if (successRate >= MAX_ERROR_RATE) {
+      gradient = `
+        conic-gradient(
+          var(--green) 0deg,
+          var(--green) ${orangeStart}deg,
+          var(--orange) ${orangeStart}deg,
+          var(--orange) ${orangeEnd}deg,
+          var(--green) ${orangeEnd}deg,
+          var(--green) ${successSize}deg,
+          var(--red) ${successSize}deg,
+          var(--red) 360deg
+        )
+      `;
+    } else {
+      gradient = `
+        conic-gradient(
+          var(--green) 0deg,
+          var(--green) ${redStart}deg,
+          var(--red) ${redStart}deg,
+          var(--red) ${orangeStart}deg,
+          var(--orange) ${orangeStart}deg,
+          var(--orange) ${orangeEnd}deg,
+          var(--red) ${orangeEnd}deg,
+          var(--red) ${redEnd}deg
+        )
+      `;
+    }
+
+    // Update the background of the circular diagram
+    circularDiagram.style.background = gradient;
+    console.log("Diagram updated", gradient);
+  } catch (error) {
+    console.log("Error updating diagram: ", error);
   }
 }
 
