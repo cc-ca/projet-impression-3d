2f2e7dd9b9967d2925b5a3b5df7cdbda9aec9761
unknown <maxime.herbin0545@laposte.net>
2024-05-23 10:30:12 +0200

    Update model_evaluation.py

--- 3d-printer-error-detector/model_evaluation.py
+++ 3d-printer-error-detector/model_evaluation.py
@@ -20,27 +20,26 @@ def evaluate_model():
 
 def run():
     change_color(State.WARMUP)
-    try:
-        while settings.model_thread_running:
-            if settings.capture_is_running:
-                color = evaluate_model()
-                change_color(State.OFF)
-                time.sleep(SLEEP_LED)
-                change_color(color)
-                if len(settings.history) >= (RUN_DURATION // SLEEP_INTERVAL - 1):
-                    success_count = settings.history.count("0")
-                    failure_count = settings.history.count("1")
-                    settings.error_rate = failure_count / (success_count + failure_count)
-                    if settings.error_rate >= CONFIDENCE_THRESHOLD:
-                        print(f"Threshold exceeded - Error rate: {settings.error_rate:.2%}")
-                        stop()
-                    else:
-                        print(f"No threshold exceeded - Error rate: {settings.error_rate:.2%}")
+    while settings.model_thread_running:
+        if settings.capture_is_running:
+            color = evaluate_model()
+            change_color(State.OFF)
+            time.sleep(SLEEP_LED)
+            change_color(color)
+            if len(settings.history) >= (RUN_DURATION // SLEEP_INTERVAL - 1):
+                success_count = settings.history.count("0")
+                failure_count = settings.history.count("1")
+                settings.error_rate = failure_count / (success_count + failure_count)
+                if settings.error_rate >= CONFIDENCE_THRESHOLD:
+                    print(f"Threshold exceeded - Error rate: {settings.error_rate:.2%}")
+                    stop()
                 else:
-                    print("Not enough elements in history for calculation.")
-                time.sleep(SLEEP_INTERVAL)
-            if not settings.capture_is_running and settings.current_state != State.IDLE:
-                change_color(State.IDLE)
+                    print(f"No threshold exceeded - Error rate: {settings.error_rate:.2%}")
+            else:
+                print("Not enough elements in history for calculation.")
+            time.sleep(SLEEP_INTERVAL)
+        if not settings.capture_is_running and settings.current_state != State.IDLE:
+            change_color(State.IDLE)
 
 def stop():
     settings.current_state, settings.model_thread_running, settings.capture_is_running = State.STOP, False, False
